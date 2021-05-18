7aaa68a43260abf7a293a59f6ead0a7189c1524b
langwenjing <13951760191@163.com>
2021-05-18 14:08:53 +0800

    first submit

---
+++ pages/index/index.vue
@@ -0,0 +1,60 @@
+<template>
+	<view class="content">
+		<image class="logo" src="https://cdn.ggac.com/media/work/image/2021/05/3d444952-b798-11eb-8733-0242c0a84002.jpg"></image>
+		<view class="text-area">
+			<text class="title">欢迎来到猪胖食堂，请用心选择晚餐哦~</text>
+		</view>
+		 <navigator url="/pages/list/list"><view class="fat-btn">开启美食之旅吧</view></navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.fat-btn{
+		padding: 20rpx 25rpx;
+		border: 1rpx solid #ffe300;
+		border-radius: 50rpx;
+		color: #ffe300;
+	}
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		
+		width: 400rpx;
+		height: 300rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+		margin-bottom: 50rpx;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ pages/list/list.vue
@@ -0,0 +1,43 @@
+<template>
+	<view class="fat-list-wrapper">
+		<van-tabs :active="active" @change="onChange">
+		  <van-tab title="面条">
+			  <view class="list-all">
+				  <van-card
+				    desc="美味面条"
+				    title="西红柿鸡蛋面"
+				    :thumb="imageURL"
+				  />
+			  </view>
+		  </van-tab>
+		  <van-tab title="炒菜">内容 2</van-tab>
+		  <van-tab title="火锅">内容 3</van-tab>
+		  <van-tab title="烤肉">内容 4</van-tab>
+		</van-tabs>
+		
+	</view>
+</template>
+
+<script>
+	import vancard from '@/wxcomponents/vant/card/index';
+	import vantab from '@/wxcomponents/vant/tab/index';
+	import vantabs from '@/wxcomponents/vant/tabs/index';
+	export default {
+		components: {
+		 "van-card":vancard,
+		 "van-tab":vantab,
+		 "van-tabs":vantabs
+		},
+		data() {
+			return {
+				active:0,
+				imageURL:'https://cdn.ggac.com/media/work/image/2021/05/3d444952-b798-11eb-8733-0242c0a84002.jpg'
+			};
+		}
+	}
+</script>
+<style lang="scss" scoped>
+	@import "@/wxcomponents/vant/common/index.wxss";
+	.fat-list-wrapper{
+	}
+</style>
